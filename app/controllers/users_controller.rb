73b7967f7180296ce10e47ec49dcf7a51be0ca51
mlgvla <mlgerardvla@gmail.com>
2020-05-26 21:35:50 -0400

    Removed binding.pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,6 @@ class UsersController < ApplicationController
     @user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
     @user.save
     session[:user_id] = @user.id
-    binding.pry
     redirect "/users"
   end
 
