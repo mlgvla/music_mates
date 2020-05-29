46815e30d41b89d99b6d1e833538ed1d07cf698c
mlgvla <mlgerardvla@gmail.com>
2020-05-29 01:13:43 -0400

    Started to add Rack Flash and authorization code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  use Rack::Flash
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
@@ -23,6 +25,13 @@ class ApplicationController < Sinatra::Base
 
     def current_user
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def authentication_required
+      if !logged_in?        
+        flash[:notice] = "You must be logged in."
+        redirect '/'
+      end
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,26 +20,49 @@ class UsersController < ApplicationController
     erb :"/users/new.html"
   end
 
+  # GET: "/users/login"
+  get "/users/login" do
+    erb :"/users/login.html"
+  end
+
   # POST: /users
   post "/users" do
+    #CHECK THAT ALL FIELDS ARE FILLED IN - ELSE RETURN TO NEW PAGE - VALIDATION!!!
     @user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
     @user.save
     session[:user_id] = @user.id
-    redirect "/users"
+    #PROBABLY REDIRECT TO SHOW PAGE /users/:id
+    redirect :"/users/#{@user.id}" 
   end
 
+  post "/users/login" do
+    #CHECK THAT ALL FIELDS ARE FILLED IN - ELSE RETURN TO LOGIN PAGE - VALIDATION!!
+    @user = User.find_by(email: params[:email])
+    
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect :"/users/#{@user.id}"
+    else
+      redirect :"/users/login"
+    end
+  end
+  
   # GET: /users/5
   get "/users/:id" do
+    @user = User.find(params[:id])
     erb :"/users/show.html"
   end
 
   # GET: /users/5/edit
   get "/users/:id/edit" do
+    @user = User.find(params[:id])
+    @instruments = Instrument.all
     erb :"/users/edit.html"
   end
 
   # PATCH: /users/5
   patch "/users/:id" do
+    binding.pry
     redirect "/users/:id"
   end
 
