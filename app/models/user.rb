03e4798c1a2aa3f4bc30b4ccfa1a7d54f56fe3bb
mlgvla <mlgerardvla@gmail.com>
2020-06-04 00:45:35 -0400

    Added dependent: :destroy to has_many :user_instruments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
 
-    has_many :user_instruments
+    has_many :user_instruments, dependent: :destroy
     has_many :instruments, through: :user_instruments
 end
