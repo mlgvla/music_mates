561a03531d74eeae0fd95620e8b88db240e8c882
mlgvla <mlgerardvla@gmail.com>
2020-06-05 17:46:37 -0400

    Added presence and uniqueness validator AR macros

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
 
     has_many :user_instruments, dependent: :destroy
     has_many :instruments, through: :user_instruments
+
+    validates :first_name, :last_name, :email, :visibility, presence: true
+
+    validates :email, uniqueness: true
+
+
 end
